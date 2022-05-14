72eeb26f16e3dd14f1c5ccfa052ef59e88480254
Tabea Frenzel <tabea.frenzel@web.de>
2022-05-14 15:06:01 +0200

    [main] Day 03 - Use lower func to support different ways of writing answers.

--- days/03/main.py
+++ days/03/main.py
@@ -23,20 +23,20 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island!")
 print("Your mission is to find the hidden treasure - let's get started.")
 
-direction = input("You're at a crossroad. Where do you want to go? Type 'right' or 'left'. ")
+direction = input("You're at a crossroad. Where do you want to go? Type 'right' or 'left'. ").lower()
 
 if direction == "right":
-    action = input("After walking for some time, you hear a loud roar. Suddenly a lion shows up. What do you do? Type 'climb' to try to climb a nearby tree or 'run' to run as fast as possible. ")
+    action = input("After walking for some time, you hear a loud roar. Suddenly a lion shows up. What do you do? Type 'climb' to try to climb a nearby tree or 'run' to run as fast as possible. ").lower()
 
     if action == "climb":
         print("You made it to the top of the tree, but the lion is not leaving. Guess you'll have to stay here for a while. Game Over :(")
     else:
         print("Oh no, that was a stupid idea, the lion is much quicker than you are and gets to you in no time. Game Over :(")
 elif direction == "left":
-    action = input("You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across. ")
+    action = input("You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across. ").lower()
 
     if action == "wait":
-        door = input("You take the boat and arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? ")
+        door = input("You take the boat and arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? ").lower()
 
         if door == "red":
             print("Oh no, you enter a room full of fire. Game Over :(")
